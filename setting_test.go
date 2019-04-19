1e61746dac43d0e7326d1720c1cb8d45aa911532
Davydov Denis <dadmoscow@gmail.com>
2019-04-19 10:39:27 +0300

    Add setting module unit test

---
+++ setting_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParseSettingFromEnv(t *testing.T) {
+
+	testData := map[string]string{
+		"gc_token":         "TOKEN",
+		"gc_bot_debug":     "false",
+		"gc_check_timeout": "60",
+		"gc_redis_addr":    "127.0.0.1:6379",
+		"gc_redis_db":      "0",
+		"gc_redis_pwd":     "qwerty",
+		"gc_use_socks5":    "true",
+		"gc_socks5_user":   "user",
+		"gc_socks5_pwd":    "password",
+		"gc_socks5_addr":   "172.0.0.1:8080",
+	}
+
+	for key, value := range testData {
+		_ = os.Setenv(key, value)
+	}
+
+	testSetting := botSetting{
+		botToken:        "TOKEN",
+		botDebug:        false,
+		gcTimeout:       60,
+		dbRedisAddress:  "127.0.0.1:6379",
+		dbRedisDB:       0,
+		dbRedisPassword: "qwerty",
+		useSocksProxy:   true,
+		socksParams: struct {
+			socksAddress  string
+			socksUser     string
+			socksPassword string
+		}{socksAddress: "172.0.0.1:8080",
+			socksUser:     "user",
+			socksPassword: "password"},
+		timeoutLimit: 604800,
+	}
+
+	setting := parseSetting(loadSettingFromEnv())
+
+	if !(*setting == testSetting) {
+		t.Error("Error with compare setting")
+	}
+}
+
+func TestParseFromFile(t *testing.T) {
+	testSetting := botSetting{
+		botToken:        "BOT_TOKEN",
+		botDebug:        true,
+		gcTimeout:       60,
+		dbRedisAddress:  "127.0.0.1:6379",
+		dbRedisDB:       0,
+		dbRedisPassword: "",
+		useSocksProxy:   false,
+		timeoutLimit:    604800,
+	}
+
+	setting := parseSetting(loadSettingFromFile("setting.json"))
+
+	if !(*setting == testSetting) {
+		t.Error("Error with compare setting from file")
+	}
+}
